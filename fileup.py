99f092a4cb9341c9d11a175771fe2d9fe9aa8eeb
runarsolar <runarsolar@gmail.com>
2024-07-17 00:33:26 +0800

    some changes

--- fileup.py
+++ fileup.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, flash, request, send_file, redirect, url_for, g
+from flask import Flask, render_template, flash, request, send_file, redirect, url_for, abort
 from werkzeug.utils import secure_filename
 import os, shutil
 from urllib.parse import urlparse
 
-#$env:FLASK_APP="fileup.py";$env:FLASK_ENV="development";flask run
+#$env:FLASK_APP="fileup.py";$env:FLASK_ENV="development";flask run -h 0.0.0.0
 
 app = Flask(__name__)
 app.secret_key = 'dev'
@@ -44,11 +44,12 @@ def show_list(url):
             return send_file(path)
     except:
         return redirect('/')
-    try:
-        filedest = upload()
-    except:
-        flash('Upload Fail')
-        return redirect('/')
+    
+    #try:
+    filedest = upload()
+    #except:
+    #    flash('Upload Fail')
+    #    return redirect('/')
     if filedest:
         return redirect(request.url)
     
@@ -77,18 +78,63 @@ def show_file(dir):
         
 def upload():
     if request.method == 'POST':
+        global root
+        if 'op' in request.form:
+            if request.form['op'] == 'delete':
+                delpath = request.form['path']
+                deldir = request.form['dirname'].split(',')
+                delfile = request.form['filename'].split(',')
+                if deldir != ['']:
+                    for x in deldir:
+                        flash(os.path.join(root, delpath, x) + ' deleted.')
+                        shutil.rmtree(os.path.join(root, delpath, x))
+                if delfile != ['']:
+                    for x in delfile:
+                        flash(os.path.join(root, delpath, x) + ' deleted.')
+                        os.remove(os.path.join(root, delpath, x))
+                return 'delete'
+            elif request.form['op'] == 'rename':
+                newname = request.form['newname']
+                if newname == '':
+                    flash('No name input.')
+                    return redirect(request.url)
+                repath = request.form['path']
+                redir = request.form['dirname'].split(',')
+                refile = request.form['filename'].split(',')
+                if (len(redir) > 1) or (len(refile) > 1 ) or ((redir != ['']) and ((refile != ['']))):
+                    flash('Select just One')
+                    return redirect(request.url)
+                if redir != ['']:
+                    flash(os.path.join(root, repath, redir[0]) + ' renamed.')
+                    os.rename(os.path.join(root, repath, redir[0]), os.path.join(root, repath, newname))
+                if refile != ['']:
+                    flash(os.path.join(root, repath, refile[0]) + ' renamed.')
+                    os.rename(os.path.join(root, repath, refile[0]), os.path.join(root, repath, newname))
+                return 'rename'
+
+
+
+
         if 'disk' in request.form:
-            global root
             root = request.form['disk'] + ':/'
             return redirect('/')
-
+        
+        if 'foldername' in request.form:
+            foldername = request.form['foldername']
+            url = urlparse(request.url).path + '/'
+            folderpath = os.path.join(root, url, foldername)
+            os.makedirs(folderpath)
+            return redirect(request.url)
+        
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
+        
         file = request.files['file']
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+        
         filename = secure_filename(file.filename)
         fileup = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
@@ -96,7 +142,6 @@ def upload():
         filedest = os.path.join(root, url, filename)
         file.save(fileup)
         shutil.move(fileup, filedest)
-        flash('Upload Success')
         return filedest
     return
 
@@ -106,3 +151,4 @@ def sizedisp(num):
             return f"{num:3.1f} {unit}B"
         num /= 1024.0
     return f"{num:.1f} GB"
+    
--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 set FLASK_APP=fileup.py
 set FLASK_ENV=development
-flask run -h 0.0.0.0 -p 80
+flask run -h 0.0.0.0 -p 8000
